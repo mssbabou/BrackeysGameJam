62f64af4b5a7cb47f70a1c379551dbd414885ceb
MORTX007 <mortx007@gmail.com>
2020-07-24 12:56:36 -0400

    maybe something

--- BGJ/Assets/Scripts/CubeSpawner.cs
+++ BGJ/Assets/Scripts/CubeSpawner.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CubeSpawner : MonoBehaviour
 {
+    // Finsing half of the width of the camera viewport
     float width = Camera.main.orthographicSize * Camera.main.aspect;
 
     Transform transform;
 
     Vector3 offset;
 
+    // Serializing the prefab we want to instantiate at run time
     public GameObject prefab;
 
+    // Serializing the height of the CubeSpawner object in game
     [Range(-15f, 15f)]
     public float spawnHeight = 6f;
 
@@ -24,12 +27,13 @@ public class CubeSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
+        // Finding the random place to send the CubeSpawner to Instantiate a Minecraft Cube!!!
         offset = new Vector3(Random.Range(-width, width), spawnHeight, 0);
 
+        // Debug code
         print(offset);
 
+        // Instantiating the Minecraft cube at the right
         Instantiate(prefab, offset, Quaternion.identity);
     }
 }
