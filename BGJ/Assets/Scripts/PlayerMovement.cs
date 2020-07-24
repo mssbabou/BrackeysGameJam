d4e288157695ffe330ff65178ccb44bfa0ca5c11
Shlonkey <jonathantunstall03@gmail.com>
2020-07-24 15:49:41 +0100

    Added Camera Scriot to change ratios and size, also added player movement in x-z plane
    Additionally added ranges and some headers

--- BGJ/Assets/Scripts/PlayerMovement.cs
+++ BGJ/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script moves the player
+
 public class PlayerMovement : MonoBehaviour
 {
+    //Variable allows access to the RigidBody
+    Rigidbody rigidbody;
+
+    //Variable to allow access to the Transform
+    Transform transform;
+
+    //Vector2 to store player input
+    private Vector2 input;
+
+    //Vector2 storing the normalized input as a direction
+    private Vector2 direction;
+
+    //Variable for player movement speed
+    [Range(0.01f, 15f)]
+    public float speed;
+
+    //Vector2 containing direction * speed
+    private Vector2 velocity;
+
+    //Adjusted velocity vector by Time.deltaTime to balance movement speed
+    private Vector2 moveAmount;
+
+
+
     // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
+        //Sets the transform variable to the Transform on this GameObject
+        transform = GetComponent<Transform>();
+
+        //sets the variable rigidbody to the RigidBody on this GameObject
+        rigidbody = GetComponent<Rigidbody>();
         
+        //Sets the rigidbody to be kinematic, meaning it isnt affected by the physics engine, but can still detect collisions
+        rigidbody.isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //fixes all public variables that could cause null errors
+        FixNullErrors();
+
+        //detects player input from arrow keys and WASD, and sets the input variable to a Vector2 in range (0, 0) --> (1, 1) to allow x-z movement
+        HandleInput();
+    }
+
+    void FixNullErrors()
+    {
+        //stops the player from having a speed of 0
+        if (speed <= 0)
+        {
+            speed = 0.01f;
+        }
+    }
+
+    void HandleInput()
+    {
+
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        direction = input.normalized;
+
+        velocity = direction * speed;
+
+        moveAmount = velocity * Time.deltaTime;
+
+        //moves the player using the Vector2 moveAmount
+        transform.Translate(moveAmount);
+
     }
 }
--- BGJ/Assets/Scripts/StandadCamera.cs
+++ BGJ/Assets/Scripts/StandadCamera.cs
@@ -2,22 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script allows everyone to have the same camera size, or be able to change it, values can be made fixed once decision has been made
+
 public class StandadCamera : MonoBehaviour
 {
+    [Header("Aspect Ratio & Size")]
     [Range(0.01f, 15f)]
-    public float desiredAspect;
+    public float desiredAspect = 1.4f;
 
     [Range(0.01f, 15f)]
-    public float desiredSize;
-    public Camera camera;
+    public float desiredSize = 4.85f;
+    Camera camera;
+    
     // Start is called before the first frame update
     void Start()
     {
+        camera = GetComponent<Camera>();
 
+        FixNullErrors();
+
+        AdjustCamera();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixNullErrors()
+    {
+        //stops aspect from throwing an error
+        if (desiredAspect <= 0)
+        {
+            desiredAspect = 0.1f;
+        }
+
+        //stops size from throwing an error
+        if (desiredSize <= 0)
+        {
+            desiredSize = 0.1f;
+        }
+    }
+
+    void AdjustCamera()
     {
         //sets height
         camera.orthographicSize = desiredSize;
