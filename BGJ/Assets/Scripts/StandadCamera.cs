907b5c8f39dea11fc661b8c7d9c2e28f08fa6d37
Shlonkey <jonathantunstall03@gmail.com>
2020-07-24 14:58:22 +0100

    Added camera size script

--- BGJ/Assets/Scripts/StandadCamera.cs
+++ BGJ/Assets/Scripts/StandadCamera.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class StandadCamera : MonoBehaviour
 {
+    [Range(0.01f, 15f)]
+    public float desiredAspect;
+
+    [Range(0.01f, 15f)]
+    public float desiredSize;
     public Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //sets height
+        camera.orthographicSize = desiredSize;
+
+        //sets the ratio between height and width
+        camera.aspect = desiredAspect;
     }
 }
