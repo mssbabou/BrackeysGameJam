506f78054708f16b0411cc36c9f0b1f410abc9ff
DibbelGames <53196457+DibbelGames@users.noreply.github.com>
2020-08-03 00:21:38 +0200

    EnemyHalth+TakeDamage

---
+++ Brackeys Game Jam 2020/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public float maxHealth;
+    private float health;
+
+    public Image healthBar;
+
+    void Start(){
+        health = maxHealth;
+    }
+
+    public void TakeDamage(float amount){
+        health -= amount;
+        healthBar.fillAmount = health / maxHealth;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if(col.gameObject.tag == "Projectile"){
+            TakeDamage(FindObjectOfType<WeaponScript>().damage);
+        }
+    }
+}
--- Brackeys Game Jam 2020/Assets/Scripts/WeaponScript.cs
+++ Brackeys Game Jam 2020/Assets/Scripts/WeaponScript.cs
@@ -16,6 +16,7 @@ public class WeaponScript : MonoBehaviour
     private float offsetScale = 1.5f;
     private Vector2 direction;
     private Vector2 displacment;
+    public float damage;
     // Start is called before the first frame update
     void Start()
     {
