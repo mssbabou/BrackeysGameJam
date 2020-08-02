e7967bb14926fee2a1d15e0b81c630d3d62718a0
MORTX007 <mortx007@gmail.com>
2020-08-02 13:25:14 -0400

    Revert "Merge branch 'master' of https://github.com/mssbabou/BrackeysGameJam"
    This reverts commit 046c1961db280a2a597e1ee85770aea230d0ce40, reversing
    changes made to 8160daf7268bd3f4b219c6af5f7f9e3e3ec31071.

---
+++ Brackeys Game Jam 2020/Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    private GameObject platformHolder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformHolder = GameObject.Find("Platform Container");
+        transform.parent = platformHolder.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
