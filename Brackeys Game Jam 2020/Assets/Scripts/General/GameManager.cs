d7451e17c376bef9657259e488efc3e3ca942914
DibbelGames <53196457+DibbelGames@users.noreply.github.com>
2020-08-03 22:48:38 +0200

    Added a pause menu

--- Brackeys Game Jam 2020/Assets/Scripts/General/GameManager.cs
+++ Brackeys Game Jam 2020/Assets/Scripts/General/GameManager.cs
@@ -5,11 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    bool paused;
+
     void Start(){
         Time.timeScale = 1;
     }
 
     public void Restart(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Pause(){
+        paused = !paused;
+        if(paused)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
     }
 }
