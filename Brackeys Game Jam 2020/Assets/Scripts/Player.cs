25ee4c02cbe086e0a21b6ad460dc60afb7829966
DibbelGames <53196457+DibbelGames@users.noreply.github.com>
2020-08-03 16:33:35 +0200

    Added PlayerHealth

---
+++ Brackeys Game Jam 2020/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float maxHealth;
+    private float health;
+
+    public GameObject hitFeedback;
+
+    public Image healthBar;
+    public Text healthDisp;
+
+    void Start(){
+        health = maxHealth;
+        healthBar.fillAmount = health;
+    }
+
+    public void TakeDamage(float amount){
+        health -= amount;
+        healthBar.fillAmount = health / maxHealth;
+        healthDisp.text = health + "/" + maxHealth.ToString();
+
+        GameObject h = Instantiate(hitFeedback, transform.position, Quaternion.identity);
+        Destroy(h, 0.15f);
+
+        if(health <= 0){
+            Die();
+        }
+    }
+
+    void Die(){
+        print("You're Dead!!!");
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.tag == "Enemy"){
+            Destroy(col.gameObject);
+            TakeDamage(10);
+        }
+    }
+}
