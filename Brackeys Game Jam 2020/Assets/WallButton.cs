32dd7708723c26cbf11240723d17d6b92414b006
MORTX007 <mortx007@gmail.com>
2020-08-03 14:59:57 -0400

    Wall Button Made

---
+++ Brackeys Game Jam 2020/Assets/WallButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallButton : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer wallButton;
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            wallButton.color = Color.green;
+        }
+    }
+}
