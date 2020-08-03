e840343971ec66d59d3812cae3f5e5464b6893a4
Shlonkey <jonathantunstall03@gmail.com>
2020-08-03 22:15:14 +0100

    tried to fix button

--- Brackeys Game Jam 2020/Assets/Scripts/Doors&Buttons/WallButton.cs
+++ Brackeys Game Jam 2020/Assets/Scripts/Doors&Buttons/WallButton.cs
@@ -4,18 +4,58 @@ using UnityEngine;
 
 public class WallButton : MonoBehaviour
 {
-    [SerializeField] SpriteRenderer wallButton;
+    [SerializeField] SpriteRenderer wallButtonSpriteRenderer;
     public delegate void ActivateButton();
     public delegate void DeactivateButton();
+
+    [SerializeField]
+    private bool triggered = false;
+
+    [SerializeField]
+    private float activationPeriodLength;
 
     public ActivateButton ButtonActivated;
     public DeactivateButton ButtonDeactivated;
 
     void OnMouseOver()
     {
-        if (Input.GetMouseButtonDown(0))
+        print("mouse over button");
+        if (Input.GetMouseButtonDown(0) && !triggered)
         {
-            wallButton.color = Color.green;
+            print("Triggered buton activate");
+            StartCoroutine(ButtonActivation());
+        }
+    }
+
+    private IEnumerator ButtonActivation()
+    {
+        
+
+        if (ButtonActivated != null)
+        {
+            ButtonActivated();
+
+            wallButtonSpriteRenderer.color = Color.green;
+
+            triggered = true;
+
+            yield return new WaitForSeconds(activationPeriodLength);
+            StartCoroutine(ButtonDeactivation());
+        }
+
+    }
+
+    private IEnumerator ButtonDeactivation()
+    {
+        if (ButtonDeactivated != null)
+        {
+            ButtonDeactivated();
+
+            wallButtonSpriteRenderer.color = Color.red;
+
+            triggered = false;
+
+            yield break;
         }
     }
 }
