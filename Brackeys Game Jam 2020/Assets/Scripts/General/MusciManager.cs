283df1cacc4f7fb39596d529bddd98657c152163
DibbelGames <53196457+DibbelGames@users.noreply.github.com>
2020-08-06 20:05:41 +0200

    Music should be working now

---
+++ Brackeys Game Jam 2020/Assets/Scripts/General/MusciManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusciManager : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject inGame;
+    public GameObject boss;
+
+    public bool mute;
+    
+    void Update(){
+        string sceneName = SceneManager.GetActiveScene().name;
+        if(sceneName.Contains("Level")){
+            inGame.SetActive(true);
+            boss.SetActive(false);
+            menu.SetActive(false);
+        }else if(sceneName.Contains("Boss")){
+           boss.SetActive(true);
+           inGame.SetActive(false);
+           menu.SetActive(false);
+        }else{
+            menu.SetActive(true);
+            boss.SetActive(false);
+            inGame.SetActive(false);
+        
+        menu.GetComponent<AudioSource>().mute = mute;
+        inGame.GetComponent<AudioSource>().mute = mute;}
+        boss.GetComponent<AudioSource>().mute = mute;
+    }
+}
