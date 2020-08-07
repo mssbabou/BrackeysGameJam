c6a6c98c9d1164c4e6e03d98abad019cd9a1c942
DibbelGames <53196457+DibbelGames@users.noreply.github.com>
2020-08-07 15:25:17 +0200

    not finished laser eyes

--- Brackeys Game Jam 2020/Assets/Scripts/Enemy/Laser.cs
+++ Brackeys Game Jam 2020/Assets/Scripts/Enemy/Laser.cs
@@ -7,12 +7,18 @@ public class Laser : MonoBehaviour
     public float speed;
 
     private Transform target;
+    Vector2 pos;
 
     void Start(){
         target = GameObject.Find("Player").transform;
+        pos = target.position;
     }
 
     void Update(){
-        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), target.position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), pos, speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        Destroy(gameObject);
     }
 }
