67253ea2450841bf67daf06afb43e8675b16889d
DibbelGames <53196457+DibbelGames@users.noreply.github.com>
2020-08-07 20:05:18 +0200

    timer ui kind of implemented

--- Brackeys Game Jam 2020/Assets/Scripts/General/LevelTimeScript.cs
+++ Brackeys Game Jam 2020/Assets/Scripts/General/LevelTimeScript.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelTimeScript : MonoBehaviour
 {
+    public Text timeDisp;
+
     [SerializeField]
     private GameObject finishDoor;
     private float startTime;
@@ -12,20 +15,25 @@ public class LevelTimeScript : MonoBehaviour
     private float currentPlayTime;
     
     [HideInInspector]
-    public float timeToCompleteLevel;
+    public static float timeToCompleteLevel;
 
     private bool stop = false;
     // Start is called before the first frame update
     void Start()
     {
-        finishDoor.GetComponent<FinishDoor>().PlayerFinishedLevel += StopTimeKeeper;
         startTime = Time.time;
+        if(finishDoor != null){
+            finishDoor.GetComponent<FinishDoor>().PlayerFinishedLevel += StopTimeKeeper;
+        }
     }
     void Update()
     {
         if (!stop)
         {
             currentPlayTime = Time.time;
+        }
+        if(timeDisp != null){
+            timeDisp.text = "Needed Time: " + timeToCompleteLevel + " s";
         }
     }
 
